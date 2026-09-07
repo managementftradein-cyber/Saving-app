6af0d4e32c5fab0c5d8a1499169cf0e4a7fa00cb
managementftradein-cyber <managementftradein@gmail.com>
2026-09-07 18:09:55 -0300

    Delete app directory

--- app/onboarding/layout.tsx
+++
@@ -1,5 +0,0 @@
-import AppShell from "@/components/app-shell";
-
-export default function OnboardingLayout({ children }: { children: React.ReactNode }) {
-  return <AppShell>{children}</AppShell>;
-}
