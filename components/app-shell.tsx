7a5ac6ff2676517071542cfdda0498bed6aaf758
managementftradein-cyber <managementftradein@gmail.com>
2026-09-07 18:10:09 -0300

    Delete components directory

--- components/app-shell.tsx
+++
@@ -1,7 +0,0 @@
-export default function AppShell({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="app-shell-bg">
-      <div className="app-shell-frame">{children}</div>
-    </div>
-  );
-}
